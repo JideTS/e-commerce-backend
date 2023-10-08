05b07398f323ed43ffd3f0b6e13fc2e208d4e321
Shaduruk <jide@tidjani-serpos.com>
2023-10-08 01:10:50 -0400

    set up fields for sequelize models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,49 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      // prevents null values
+      allowNull: false,
+      // will only allow decimal characters
+      validate: {
+        isDecimal: true,
+      },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      // prevents null values
+      allowNull: false,
+      // will only allow decimal characters
+      validate: {
+        isInt: true,
+      },
+      defaultValue: 10
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `category` model, which we set in `Category.js` as its `modelName` property
+        model: 'category',
+        key: 'id',
+      },
+    },
+
   },
   {
     sequelize,
